b505322742ecaca57803be5bce11c98bedd6e4df
sashedher <sashedher.14@gmail.com>
2022-09-25 22:39:10 +0530

    parse individual url rdf file

--- ResultOntoIndiv.py
+++ ResultOntoIndiv.py
@@ -1,37 +1,14 @@
 from rdflib import Graph
 
-def load_loc_info(id):
+def load_loc_info(id,type):
     g_aBox = Graph()
-    i=0
-    url='https://www.geonames.org/'+str(id)+'/about.rdf'
+
+    url='https://www.geonames.org/'+str(id)+'/'+type+'.rdf'
     try:
         g_aBox.parse(url)
         # print("{}:  {}".format(i,url))
     except:
-        print("This feature does not have about.rdf or invalid id")
+        print("This feature does not have this type of rdf file or invalid id")
 
-    url = 'https://www.geonames.org/'+str(id)+'/nearby.rdf'
-    try:
-        g_aBox.parse(url)
-        # print("{}:  {}".format(i,url))
-    except:
-        print("This feature does not have nearby.rdf or invalid id")
-
-
-    url = 'https://www.geonames.org/'+str(id)+'/neighbours.rdf'
-    try:
-        g_aBox.parse(url)
-        # print("{}:  {}".format(i,url))
-    except:
-        print("This feature does not have neighbour.rdf or invalid id")
-
-
-    url = 'https://www.geonames.org/'+str(id)+'/contains.rdf'
-    try:
-        g_aBox.parse(url)
-        # print("{}:  {}".format(i,url))
-    except:
-        print("This feature does not have contains.rdf or invalid id")
-    
     return g_aBox
 
