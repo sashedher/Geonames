b7dec3dbd30f5d828dcfd1af6932223280c26008
sashedher <sashedher.14@gmail.com>
2022-11-18 01:26:21 +0530

    funtion to return response from openAi

--- openAI.py
+++ openAI.py
@@ -4,15 +4,14 @@ import apikey
 openai.api_key = apikey.key_openAI
 
 # get prebuild string
-_prompt = ""
-
-
-response = openai.Completion.create(
-  model="text-davinci-001",
-  prompt=_prompt,
-  temperature=1.0,
-  max_tokens=128,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=1
-)
+def generate_sentence(_prompt):
+  response = openai.Completion.create(
+    model="text-davinci-001",
+    prompt=_prompt,
+    temperature=1.0,
+    max_tokens=128,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=1
+  )
+  return response["choices"][0]["text"]
