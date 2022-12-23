4fd1d368ac8bf3eb228cbd2e202717640aed2305
sashedher <sashedher.14@gmail.com>
2022-12-23 12:27:55 +0530

    model configuration changed

--- openAI.py
+++ openAI.py
@@ -5,13 +5,14 @@ openai.api_key = apikey.key_openAI
 
 # get prebuild string
 def generate_sentence(_prompt):
+  _prompt= "convert the following sentence into natural language sentence.\n" +_prompt
   response = openai.Completion.create(
-    model="text-davinci-001",
+    model="text-davinci-003",
     prompt=_prompt,
-    temperature=1.0,
-    max_tokens=128,
+    temperature=0,
+    max_tokens=256,
     top_p=1,
     frequency_penalty=0,
-    presence_penalty=1
+    presence_penalty=0
   )
   return response["choices"][0]["text"]
