96b45ad99f2fc10f92647ff441badab2006f4853
sashedher <sashedher.14@gmail.com>
2022-12-25 23:12:32 +0530

    rectified and updated modal submit action

--- static/js/script.js
+++ static/js/script.js
@@ -27,7 +27,8 @@ function getRespone(event, formData, form) {
       } else {
         output.innerHTML = 'An error occurred during the upload. Try again.';
       }
-    };   var form = document.getElementById('my-form');
+    };   
+    var form = document.getElementById('my-form');
     var formData = new FormData(form);
     form.addEventListener('submit', (event)=>{
         getRespone(event, formData, form);
@@ -37,12 +38,43 @@ function getRespone(event, formData, form) {
     xhr.send(formData);
 }
 
-function closePopup(){
+function closePopup(linkText){
+    var city = document.getElementById('cityName');
+    var title = document.getElementById('modal-title');
+    city.value = linkText;
+    title.innerText = linkText;
+}
+
+function submitData(){
     var form = document.getElementById('modal-form');
     var city = document.getElementById('cityName');
     var formData = new FormData(form);
     formData.append("city", city.value);
-    form.addEventListener('submit', (event, formData, form) =>{
-        getRespone(event, formData);
-    });
+    getData(formData, form);
 }
+
+function getData(formData, form) {
+    // event.preventDefault();
+
+    var output = document.getElementById('output');
+    
+    output.innerHTML = '<img style="margin-top:30px;" class="mx-auto d-block" src="../static/img/loading.gif"/>';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', form.action, true);
+
+
+    // Set up a handler for when the task for the request is complete.
+    xhr.onload = function () {
+    if (xhr.status === 200) {
+        output.innerHTML = xhr.responseText;
+      } else {
+        output.innerHTML = 'An error occurred during the upload. Try again.';
+      }
+    };
+
+    // Send the data.
+    xhr.send(formData);
+}
+
+
